48b9a8bff2a33d5fe38204a678cf8296f49961dd
Nyxze <manach.alexy@gmail.com>
2022-01-05 13:52:24 +0100

    Mise en place de guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './component/pages/home/home.component';
 import { LoginComponent } from './component/pages/login/login.component';
 import { SecureComponent } from './component/pages/secure/secure.component';
+import { AuthGuard} from './guards/auth.guard';
 
 const routes: Routes = [
 {path:'home', component: HomeComponent},
 {path:'login', component: LoginComponent},
-{path:'secure', component: SecureComponent},
+{path:'secure', component: SecureComponent, canActivate:[AuthGuard]},
 {path:'', redirectTo:'/home', pathMatch:'full'}
 ];
 
---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private security: AuthService){}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.security.user.isAuthenticated){
+      return true;
+    }else{
+      return false
+    }
+  }
+  
+}
