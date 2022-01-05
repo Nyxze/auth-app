1201235d00302803e348852b3e8dd0db7143d4f6
Nyxze <manach.alexy@gmail.com>
2022-01-05 09:37:24 +0100

    auth-app-base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './comnponent/pages/home/home.component';
+import { LoginComponent } from './comnponent/pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
